50322d18d52a947225079e4ea564b80b12acfc41
Chad Rempp <crempp@gmail.com>
2015-07-31 06:19:55 +0000

    Update wsgi.py to support refactored app structure

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,10 @@
-from mdweb import app
+from mdweb import MDSite
+
+app = MDSite(
+    'MyTestSite',  # Site name, this gets passed in as the Flask name
+    config_filename='TestSiteConfig.py',  # Config file name for the site
+    app_options={}  # Flask options that will be passed through to the Flask() constructor
+)
 
 if __name__ == "__main__":
     app.run()
