079ccf7533e86148ad03a42935facdcf733bbd93
Chad Rempp <crempp@gmail.com>
2016-02-27 01:17:35 -0800

    Pylint cleanup of test_navigationbaseitem.py and Exception.py

--- mdweb/Exceptions.py
+++ mdweb/Exceptions.py
@@ -7,25 +7,42 @@ class ConfigException(Exception):
 
 
 class ContentException(Exception):
+
+    """Markdown content exception."""
+
     pass
 
 
 class ContentStructureException(Exception):
+
+    """Invalid structure of content directory."""
+
     pass
 
 
 class PageMetaInfFieldException(Exception):
+
+    """Invalid field in page metadata."""
+
     pass
 
 
 class PageParseException(Exception):
+
+    """Error parsing page markdown."""
+
     pass
 
 
-# Shims for Python 3.x Exceptions
-class FileExistsError(Exception):
+class FileExistsError(Exception): # pylint: disable=W0622
+
+    """Shim for FileExistsError in Python 2.x."""
+
     pass
 
 
-class PermissionError(Exception):
+class PermissionError(Exception): # pylint: disable=W0622
+
+    """Shim for FileExistsError in Python 2.x."""
+
     pass
