0ec53a9a5c66d49e9d9eb0a6b24dde3fee44a671
Chad Rempp <crempp@gmail.com>
2020-08-17 12:21:30 -0700

    Forgot a minor piece of #74

--- mdweb/Page.py
+++ mdweb/Page.py
@@ -15,8 +15,7 @@ from mdweb.Exceptions import (
 URL_PATH_REGEX = r'^%s(?P<path>[^\0]*?)(index)?(\.md)'
 
 #: A regex for extracting meta information (and comments).
-# META_INF_REGEX = r'(/\*(?P<metainf>.*)\*/)?(?P<content>.*)'
-META_INF_REGEX = r'(```metainf(?P<metainf>.*?)```)?(?P<content>.*)'
+META_INF_REGEX = r'(^```metainf(?P<metainf>.*?)```)?(?P<content>.*)'
 
 
 class PageMetaInf(MetaInfParser):  # pylint: disable=R0903
